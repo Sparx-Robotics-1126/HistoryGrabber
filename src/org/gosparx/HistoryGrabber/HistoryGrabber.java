4ce6f453c87b26d5ef128e32983b510346e51189
Kmodos <amechler1998@gmail.com>
2014-02-24 18:50:05 -0500

    Initial commit

---
+++ src/org/gosparx/HistoryGrabber/HistoryGrabber.java
@@ -0,0 +1,9 @@
+package org.gosparx.HistoryGrabber;
+
+/**
+ * This is a command line application that will automatically grab various files from the cRIO. You can modify this for your needs.
+ * @author Alex - Team 1126 - Kmodos
+ */
+public class HistoryGrabber {
+
+}
